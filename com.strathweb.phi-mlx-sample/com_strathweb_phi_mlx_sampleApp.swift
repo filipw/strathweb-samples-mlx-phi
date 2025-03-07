7eb75f37f1b6379ed01c48b7afbf3d50708bb2a9
filipw <filip@strathweb.com>
2025-03-07 14:17:03 +0100

    Initial Commit

---
+++ com.strathweb.phi-mlx-sample/com_strathweb_phi_mlx_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_strathweb_phi_mlx_sampleApp.swift
+//  com.strathweb.phi-mlx-sample
+//
+//  Created by Filip W on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct com_strathweb_phi_mlx_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
